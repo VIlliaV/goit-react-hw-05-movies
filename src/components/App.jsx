2ea076160e76a78cab739bd19a2cd777d372d68d
Illia <illia.vasiliev@gmail.com>
2023-04-05 01:21:49 +0300

    fix route undefind

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,14 @@ import Layout from './Layout/Layout';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/*" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:movieId" element={<MovieDetails />}>
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<Reviews />} />
         </Route>
+        <Route path="*" element={<Home />} />
       </Route>
     </Routes>
   );
