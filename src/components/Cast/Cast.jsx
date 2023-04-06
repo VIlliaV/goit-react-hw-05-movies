8b84f3177d8fdffd166c32daf96a2c5650768b44
Illia <illia.vasiliev@gmail.com>
2023-04-06 17:16:57 +0300

    start cast

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,8 +1,53 @@
+import { Loader } from 'components/Loader/Loader';
+import { useEffect, useRef, useState } from 'react';
+import { Toaster, toast } from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
+import { getImage, getResponseDetails } from 'utils/api';
+
 export const Cast = () => {
-  console.log('object');
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
+  const [pending, setPending] = useState(true);
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      getResponseDetails(movieId, '/credits')
+        .then(response => {
+          setCast(response.data?.cast);
+        })
+        .catch(error => toast.error(`Sorry, we can get a cast: ${error}`))
+        .finally(setPending(false));
+    }
+    return;
+  }, [movieId]);
+
   return (
     <div>
-      <h2>HOOPOOLLLLLL</h2>Cast
+      {pending ? (
+        <Loader />
+      ) : (
+        <ul>
+          {console.log('cast RENDER', cast, pending, movieId)}
+          {cast.length &&
+            cast.map(actor => {
+              const { cast_id, character, name, profile_path } = actor;
+              return (
+                <li key={cast_id}>
+                  <h3>{name}</h3>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${profile_path}`}
+                    alt={name}
+                  />
+                  <p>{character}</p>
+                </li>
+              );
+            })}
+        </ul>
+      )}
+      <Toaster />
     </div>
   );
 };
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef, useState } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 import { Link, Outlet, useParams } from 'react-router-dom';
+
+import { Loader } from 'components/Loader/Loader';
 import { getResponseDetails, getImage } from 'utils/api';
 import noImg from '../../images/noimage.png';
-import { Loader } from 'components/Loader/Loader';
+
 import { Container } from './MovieDetails.styled';
 
 export const MovieDetails = () => {
   const { movieId } = useParams();
+
   const [movie, setMovie] = useState({});
   const [pending, setPending] = useState(true);
   const [img, setImg] = useState(null);
@@ -21,10 +24,7 @@ export const MovieDetails = () => {
     if (poster_path)
       getImage(poster_path)
         .then(() => {
-          console.log('askPikt');
-          setTimeout(() => {
-            setImg(`https://image.tmdb.org/t/p/w500${poster_path}`);
-          }, 7000);
+          setImg(`https://image.tmdb.org/t/p/w500${poster_path}`);
         })
         .catch(error => {
           setImg(noImg);
@@ -36,34 +36,31 @@ export const MovieDetails = () => {
 
     if (firstRender.current) {
       firstRender.current = false;
-      // console.log('🚀 ~ !movie.id:', !movie.id);
 
       getResponseDetails(movieId)
         .then(response => {
-          // console.log('object :>> ', response.status);
           setMovie(response.data);
         })
         .catch(error => {
           toast.error(`${error.message}`);
         })
         .finally(() => {
-          // console.log('finished');
-          // pending.current = false;
           setPending(false);
         });
     }
-    // console.log('return');
+
     return;
   }, [movieId, poster_path]);
 
   return (
     <Container>
+      {console.log('RENDER')}
       {pending ? (
-        <p>load</p>
+        <Loader />
       ) : (
         movie.id && (
           <div>
-            {console.log('RENDER', movie.id, pending, movieId, img, isImg)}
+            {/* {console.log('RENDER', movie.id, pending, movieId, img, isImg)} */}
             <button type="button">back</button>
             <div className="movie_head">
               <div className="movie_head_img">
@@ -76,7 +73,7 @@ export const MovieDetails = () => {
 
               <div className="movie_head_about">
                 <h1>{original_title}</h1>
-                <p>{vote_average}</p>
+                <p>{`User Score:  ${(vote_average * 10).toFixed(0)}%`}</p>
                 <h2>Overview</h2>
                 <p>{overview}</p>
                 <h3>Genres</h3>
